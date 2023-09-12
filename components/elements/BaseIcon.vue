43b5a7364543ebe017c887e1ee73fd8753d2f7cb
Yakin Nikita <yakin95@gmail.com>
2023-09-12 18:06:51 +0500

    feat: Icon sprite

---
+++ components/elements/BaseIcon.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+defineProps({
+	name: {
+		type: String as PropType<
+			| 'bar'
+			| 'mysql'
+			| 'laravel'
+			| 'php'
+			| 'scss'
+			| 'css'
+			| 'html'
+			| 'javascript'
+			| 'typescript'
+			| 'nuxt'
+			| 'vue'
+			| 'telegram'
+			| 'phone'
+			| 'email'
+			| 'instagram'
+			| 'github'
+			| 'whatsapp'
+			| 'vk'
+			| 'www'
+			| 'link'
+			| 'arrow-badge-up'
+			| 'arrow-badge-down'
+		>,
+		required: true
+	}
+});
+</script>
+
+<template>
+	<svg viewBox="0 0 32 32" :class="`icon icon--${name}`">
+		<use :xlink:href="`/images/sprites.svg#${name}`" />
+	</svg>
+</template>
+
+<style scoped>
+.icon {
+	width: 32px;
+	aspect-ratio: 1 /1;
+}
+</style>
