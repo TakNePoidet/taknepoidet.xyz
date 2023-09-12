1797908c4dd3044285abe0b04c8d9c43ca5245db
Yakin Nikita <yakin95@gmail.com>
2023-09-12 18:28:13 +0500

    perf: Named pages

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,11 @@ import ThePage from '~/components/ThePage.vue';
 import TheDevelopmentSkills from '~/components/sections/TheDevelopmentSkills.vue';
 import SectionHero from '~/components/sections/SectionHero.vue';
 import SectionContacts from '~/components/sections/SectionContacts.vue';
-import { computed } from '#imports';
+import { computed, definePageMeta, Page } from '#imports';
 
+definePageMeta({
+	name: Page.Home
+});
 const host = useHost();
 const cover = computed(() => `${host.value}/images/cover.jpg`);
 const canonical = computed(() => host.value);
